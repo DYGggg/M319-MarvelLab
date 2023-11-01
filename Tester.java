7d7bb4e2ace3dce9dfa745c1c8eeb27a1a5ba280
DYGggg <jeff.peng0702@gmail.com>
2023-11-01 14:31:25 -0500

    ideas

--- Tester.java
+++ Tester.java
@@ -1,3 +1,7 @@
 public class Tester {
-    //askdbajkdfbqejfbjs
+    // Restaurant: 3 class for food, 1 class for toString()
+    // street fighter game (add custom features)
+    // 
+    //
+    //
 }
